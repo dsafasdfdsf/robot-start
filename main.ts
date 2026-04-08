9a9af4888463cd27de56ea10c50e909c994685b1
dsafasdfdsf <31hchristensen@kimberly.k12.wi.us>
2026-04-08 14:52:26 -0500

    stuff

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ input.onButtonPressed(Button.A, function () {
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
 input.onButtonPressed(Button.B, function () {
-    music.play(music.stringPlayable("E G F E A G E E ", 120), music.PlaybackMode.UntilDone)
+	
 })
 basic.showString("Hello!")
 basic.forever(function () {
